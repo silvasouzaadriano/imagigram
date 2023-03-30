c1975414ae3b0c189a5da47a6d2deca28e0e0025
adriano.souza <adriano.souza@zpesystems.com>
2023-03-30 19:48:18 -0300

    feat: Created Feed component

--- components/Main.js
+++ components/Main.js
@@ -1,10 +1,16 @@
 import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { fetchUser} from '../redux/actions';
+
+import Feed from './main/Feed';
+
+const Tab = createMaterialBottomTabNavigator();
 
 const Main = (props) => {
   useEffect(() => {
@@ -12,9 +18,21 @@ const Main = (props) => {
   }, []);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Bem vindo {props.currentUser?.name}!</Text>
-    </View>
+    <Tab.Navigator
+      initialRouteName='Feed'
+      backBehavior='initialRoute'
+      labeled={false}
+    >
+      <Tab.Screen 
+        name='Feed' 
+        component={Feed} 
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name='newspaper-variant' size={26} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   )
 }
 
