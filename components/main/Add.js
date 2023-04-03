d8cfcfec88cad3f00f6a616ff09e960738347872
adriano.souza <adriano.souza@zpesystems.com>
2023-04-03 19:18:10 -0300

    Refactoring Add component

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,35 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Camera, CameraType } from 'expo-camera';
 import { Text, Button, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function Add() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
   const [type, setType] = useState(CameraType.back);
-  const [permission, requestPermission] = Camera.useCameraPermissions();
 
-  if (!permission) {
-    // Camera permissions are still loading
+  useEffect(() => {
+    (async () => {
+      const cameraRequest = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraRequest.status === 'granted');
+      const galleryRequest = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      setHasGalleryPermission(galleryRequest.status === 'granted');
+    })();
+  }, []);
+  
+
+  if (hasCameraPermission === null || hasGalleryPermission === null) {
     return <View />;
   }
-
-  if (!permission.granted) {
-    // Camera permissions are not granted yet
-    return (
-      <View style={{ flex: 1 }}>
-        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-        <Button onPress={requestPermission} title="grant permission" />
-      </View>
-    );
+  if (hasCameraPermission === false || hasGalleryPermission === null) {
+    return <Text>No access to camera</Text>;
   }
 
-  function toggleCameraType() {
-    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
-  }
+ 
 
   return (
     <View style={{ flex: 1 }}>
       <View style={{ flex: 1, flexDirection: 'row' }}>
         <Camera style={{ flex: 1, aspectRatio: 1 }} type={type} ratio={'1:1'} />
-        <Button title='Flip Camera' onPress={toggleCameraType} />
+      </View>
+      <View style={{
+        flex: 1, 
+        flexDirection: 'row', 
+        justifyContent: 'center', 
+        alignItems: 'flex-end',
+        marginBottom: '1rem'
+      }}>
+        <Button 
+          title='Flip Camera'
+          onPress={() => {
+            setType(
+              type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+            );
+          }}
+        />
       </View>
     </View>
   );
