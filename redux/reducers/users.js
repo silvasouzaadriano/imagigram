4603bfc75621631a71dfecde557901cf4264c639
adriano.souza <adriano.souza@zpesystems.com>
2023-04-20 21:03:41 -0300

    feat: Started Redux for Like and Dislike; Refactored fetchUsersFollowingPosts getting uid

--- redux/reducers/users.js
+++ redux/reducers/users.js
@@ -1,6 +1,8 @@
 import {
   USERS_DATA_CHANGE,
   USERS_POST_CHANGE,
+  USERS_LIKE_CHANGE,
+  USERS_LIKE_COUNT_CHANGE,
   CLEAR_DATA
 } from '../constants';
 
@@ -22,6 +24,30 @@ const users = (state = initialState, action) => {
         ...state,
         usersFollowingLoaded: state.usersFollowingLoaded + 1,
         feed: [...state.feed, ...action.posts],
+      };
+    case USERS_LIKE_CHANGE:
+      return {
+        ...state,
+        feed: state.feed.map((post) =>
+          post.id == action.postId
+            ? {
+                ...post,
+                currentUserLike: action.currentUserLike,
+              }
+            : post
+        ),
+      };
+    case USERS_LIKE_COUNT_CHANGE:
+      return {
+        ...state,
+        feed: state.feed.map((post) =>
+          post.id == action.postId
+            ? {
+                ...post,
+                likes: action.likes,
+              }
+            : post
+        ),
       };
     case CLEAR_DATA:
       return initialState;
